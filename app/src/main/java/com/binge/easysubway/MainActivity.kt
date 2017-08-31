5a7f8df939281ffa75be82714ee4c8862058311b
tian.bin <tian.bin@xiaoyi.com>
2017-08-31 16:10:40 +0800

    添加kotlin支持

---
+++ app/src/main/java/com/binge/easysubway/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.binge.easysubway
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tv.text="哈哈"
+    }
+}
